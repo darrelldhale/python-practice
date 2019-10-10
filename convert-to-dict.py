e5d028a82b6058152e816f03ed5e8abbed5c009b
Darrell Hale <look585carbon@gmail.com>
2019-10-10 17:24:27 +0000

    Added a command line tool to convert json to dict.

---
+++ convert-to-dict.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.7
+"""
+Command line tool to convert json to python dict.
+"""
+
+import argparse
+import json
+import pprint
+
+pp = pprint.PrettyPrinter()
+
+def convert(file):
+    with open(file, 'r') as output_file:
+        json_file = json.load(output_file)
+       # pp = pprint.PrettyPrinter()
+        pp.pprint(json_file)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Json-to-Dict')
+
+    parser.add_argument('file', help='a json file to convert')
+
+    args = parser.parse_args()
+    convert(args.file)
